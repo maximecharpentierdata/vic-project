ad239f46a7b03eba8d299a5b829894c46e56dc97
Maxime Charpentier <pro.maxime.charpentier@gmail.com>
2022-01-20 15:04:16 +0100

    update pipeline

--- src/classification/classification.py
+++ src/classification/classification.py
@@ -1,5 +1,9 @@
 import argparse
+import datetime
 import pathlib
+from joblib import dump
+import json
+import os
 
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
@@ -37,6 +41,10 @@ def parse_arguments():
         "--no_clustering", dest="no_clustering", action=argparse.BooleanOptionalAction
     )
     argument_parser.set_defaults(no_clustering=False)
+    argument_parser.add_argument(
+        "--no_df", dest="no_df", action=argparse.BooleanOptionalAction
+    )
+    argument_parser.set_defaults(use_df=False)
 
     args = argument_parser.parse_args()
     return args
@@ -70,22 +78,43 @@ def run_classification(X_train, y_train, model_name):
 
 
 def evaluate_model(model, X_train, y_train, X_test, y_test):
-    print("Train accuracy: ", model.score(X_train, y_train))
-    print("Test accuracy: ", model.score(X_test, y_test))
+    train_accuracy = model.score(X_train, y_train)
+    test_accuracy = model.score(X_test, y_test)
+    print("Train accuracy: ", train_accuracy)
+    print("Test accuracy: ", test_accuracy)
+
+
+def save_experiment(experiments_path, model, clustering_model, final_df, params, date):
+    path = experiments_path / (date + f"_{params['classification_model']}")
+    os.makedirs(path)
+
+    params["images_path"] = params["images_path"].__str__()
+    params["labels_path"] = params["labels_path"].__str__()
+    params["models_path"] = params["models_path"].__str__()
+    params["final_data_path"] = params["final_data_path"].__str__()
+
+    with open(path / "params.json", "w") as file:
+        json.dump(params, file)
+    dump(model, path / "model.joblib")
+    dump(clustering_model, path / "clustering_model.joblib")
+    final_df.to_csv(path / "final_df.csv", index=False)
 
 
 if __name__ == "__main__":
-    FINAL_DATA_PATH = pathlib.Path("./data/final/")
+    CACHE_DATA_PATH = pathlib.Path("./data/cache/")
     INTERIM_DATA_PATH = pathlib.Path("./data/interim/")
     MODELS_PATH = pathlib.Path("./models/")
+    EXPERIMENTS_PATH = pathlib.Path("./experiments/")
 
     args = parse_arguments()
+
+    date = datetime.datetime.today().strftime("%m_%d_%H_%M_%S")
 
     params = dict(
         images_path=INTERIM_DATA_PATH / "images",
         labels_path=INTERIM_DATA_PATH / "labels.csv",
         models_path=MODELS_PATH,
-        final_data_path=FINAL_DATA_PATH,
+        final_data_path=CACHE_DATA_PATH,
         segmentation=args.segmentation,
         descriptors_proportion=args.descriptors_proportion,
         clustering_method=args.clustering_method,
@@ -93,11 +122,13 @@ if __name__ == "__main__":
         do_clustering=not args.no_clustering,
         classification_model=args.classification_model,
         n_data=args.n_data,
+        use_df=args.no_df,
+        date=date,
     )
 
     show_params(params)
 
-    final_df = run_pipeline(params)
+    final_df, clustering_model = run_pipeline(params)
 
     X_train, X_test, y_train, y_test, scaler = prepare_data_for_training(
         final_df.drop("path", axis=1)
@@ -105,3 +136,5 @@ if __name__ == "__main__":
 
     model = run_classification(X_train, y_train, params["classification_model"])
     evaluate_model(model, X_train, y_train, X_test, y_test)
+
+    save_experiment(EXPERIMENTS_PATH, model, clustering_model, final_df, params, date)
--- src/data_preparation/pipeline.py
+++ src/data_preparation/pipeline.py
@@ -12,43 +12,54 @@ from src.data_preparation.features import (
 
 
 def run_pipeline(params):
-    print("Making segmentation & computing descriptors...")
-    descriptors_list = run_preprocessing(
-        params["images_path"],
-        params["descriptors_proportion"],
-        params["segmentation"],
-        params["n_data"],
-    )
-
-    print("Making clustering...")
-    if params["do_clustering"]:
-        clustering_model = make_clustering(
-            descriptors_list, params["n_clusters"], params["clustering_method"]
-        )
-        if not params["models_path"].exists():
-            os.makedirs(params["models_path"])
-        dump(
-            clustering_model,
-            params["models_path"]
-            / f"clustering_{params['clustering_method']}_{params['n_clusters']}_{params['segmentation']}.joblib",
-        )
-    else:
+    if params["use_df"]:
         clustering_model = load(
             params["models_path"]
             / f"clustering_{params['clustering_method']}_{params['n_clusters']}_{params['segmentation']}.joblib"
         )
+        final_df = pd.read_csv(
+            params["final_data_path"]
+            / f"df_{params['clustering_method']}_{params['n_clusters']}_{params['segmentation']}.csv",
+        )
+        return final_df, clustering_model
+    else:
+        print("Making segmentation & computing descriptors...")
+        descriptors_list = run_preprocessing(
+            params["images_path"],
+            params["descriptors_proportion"],
+            params["segmentation"],
+            params["n_data"],
+        )
 
-    print("Extracting features...")
-    features = extract_features(descriptors_list, clustering_model)
+        print("Making clustering...")
+        if params["do_clustering"]:
+            clustering_model = make_clustering(
+                descriptors_list, params["n_clusters"], params["clustering_method"]
+            )
+            if not params["models_path"].exists():
+                os.makedirs(params["models_path"])
+            dump(
+                clustering_model,
+                params["models_path"]
+                / f"clustering_{params['clustering_method']}_{params['n_clusters']}_{params['segmentation']}.joblib",
+            )
+        else:
+            clustering_model = load(
+                params["models_path"]
+                / f"clustering_{params['clustering_method']}_{params['n_clusters']}_{params['segmentation']}.joblib"
+            )
 
-    labels = pd.read_csv(params["labels_path"])
-    final_df = make_final_df(features, labels)
-    if not params["final_data_path"].exists():
-        os.makedirs(params["final_data_path"])
-    final_df.to_csv(
-        params["final_data_path"]
-        / f"df_{params['clustering_method']}_{params['n_clusters']}_{params['segmentation']}.csv",
-        index=False,
-    )
+        print("Extracting features...")
+        features = extract_features(descriptors_list, clustering_model)
 
-    return final_df
+        labels = pd.read_csv(params["labels_path"])
+        final_df = make_final_df(features, labels)
+        if not params["final_data_path"].exists():
+            os.makedirs(params["final_data_path"])
+        final_df.to_csv(
+            params["final_data_path"]
+            / f"df_{params['clustering_method']}_{params['n_clusters']}_{params['segmentation']}.csv",
+            index=False,
+        )
+
+        return final_df, clustering_model
